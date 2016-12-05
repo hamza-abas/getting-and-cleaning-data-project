8f10ce9a5e0bc18032da6793d9f539d02be03af8
hamza-abas <hamzawebmaster@outlook.com>
2016-12-05 15:44:11 +0000

    getting-and-cleaning-data-project files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # getting-and-cleaning-data-project
-getting-and-cleaning-data-project 
+
+
+This repository hosts the R code and documentation files for the Data Science's track course "Getting and Cleaning data",
+
+The dataset being used is: [Human Activity Recognition Using Smartphones](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones)
+The source data for this project can be found here.](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip)
+
+
+The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. 
+The goal is to prepare tidy data that can be used for later analysis
+
+`CodeBook.md` describes the variables, the data, and any transformations or work that was performed to clean up the data.
+
+I am going To create one R script called run_analysis.R that does the following.
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names.
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+
+So first of all I have to get the data unzip the file and then Read data from the targeted files
+2. Read data from the files into the variables
+
+You can find additional information about the variables, data and transformations in the CodeBook.MD file.
+
---
+++ run_analysis.R
@@ -0,0 +1,97 @@
+﻿## The run_analysis function 
+
+#I am going To create one R script called run_analysis.R that does the following.
+
+#1. Merges the training and the test sets to create one data set.
+#2. Extracts only the measurements on the mean and standard deviation for each measurement.
+#3. Uses descriptive activity names to name the activities in the data set
+#4. Appropriately labels the data set with descriptive variable names.
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject
+
+#So first of all I have to get the data unzip the file and then Read data from the targeted files
+#2. Read data from the files into the variables
+
+#You can find additional information about the variables, data and transformations in the CodeBook.MD file.
+
+## Get the data
+
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
+#2.Unzip the file
+
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+
+#Read data from the files into the variables
+
+dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+
+#Read the Subject files
+
+dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+
+#Read Fearures files
+dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+
+##Merges the training and the test sets to create one data set
+#1.Concatenate the data tables by rows
+
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+
+#set names to variables
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+
+#Merge columns to get the data frame Data for all data
+
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+
+## Extracts only the measurements on the mean and standard deviation for each measurement
+
+#Subset Name of Features by measurements on the mean and standard deviation
+#i.e taken Names of Features with “mean()” or “std()”
+
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+
+#Subset the data frame Data by seleted names of Features
+
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+
+##Uses descriptive activity names to name the activities in the data set
+
+#Read descriptive activity names from “activity_labels.txt”
+
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+
+#Appropriately labels the data set with descriptive variable names
+
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+#Creates a second,independent tidy data set and ouput it
+
+library(dplyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+
+
+# Export the tidyData set 
+write.table(tidy, './tidy.txt',row.names=TRUE,sep='\t');
